3eb2f8d6455e9f0ea34fd9ef6ffa065bfe4b82cc
Bellash <theobellash@gmail.com>
2025-06-24 07:49:53 +0100

    initial create and Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./shared/components/layout/layout').then((m) => m.Layout),
+    children: [],
+  },
+];
